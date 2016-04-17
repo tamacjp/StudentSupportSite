d26cc64544ab07d817726712ac794251fe9cb83a
Tama Eguchi <tama.eguchi@gmail.com>
2016-04-17 16:59:13 +0900

    全データ出力API

--- project/schedule/views.py
+++ project/schedule/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+# -*- coding:utf-8 -*-
+from __future__ import unicode_literals
+from django.http import JsonResponse
+from .models import Lesson, Schedule
 
-# Create your views here.
+
+def endpoint(request):
+    # 授業
+    lessons = [lesson.toJson() for lesson in Lesson.objects.order_by('weekday', 'period')]
+
+    # スケジュール
+    schedules = [schedule.toJson() for schedule in Schedule.objects.order_by('date', 'period')]
+
+    return JsonResponse({
+        'lesson': lessons,
+        'schedule': schedules,
+    })
