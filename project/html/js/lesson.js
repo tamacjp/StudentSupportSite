8c71e56d58453613d79a0f2353d29b7ee212f82c
Tama Eguchi <tama.eguchi@gmail.com>
2016-04-18 20:10:37 +0900

    授業情報にタスク表示追加

--- project/html/js/lesson.js
+++ project/html/js/lesson.js
@@ -12,34 +12,64 @@ var makelesson = function (id) {
   }
   $area.append($title);
 
-  var $table = $('<table>').addClass('lesson-list');
-  for (var index in schedules) {
-    (function (data) {
-      if (data.lesson == id) {
-        var $row = $('<tr>').addClass('row');
-        $row.append($('<td>').addClass('date').append($('<a>').text(shortdate(new Date(data.date))).attr('href', '#list=' + data.date)));
-        $row.append($('<td>').addClass('period').text(formatperiod(data)));
-        $row.append($('<td>').addClass('teacher').text(data.teacher));
-        var $option = $('<td>').addClass('option');
-        if (data.exercises) {
-          $option.append($('<div>').addClass('exercises'));
-        }
-        if (data.test) {
-          $option.append($('<div>').addClass('test').text(data.test));
-        }
-        if (data.task) {
-          $option.append($('<div>').addClass('task').text(data.task));
-        }
-        if (data.memo) {
-          $option.append($('<div>').addClass('memo').text('メモ').on('click', function () {
-            alert(data.memo);
-          }));
-        }
-        $row.append($option);
-        $table.append($row);
+  var addtask = function (task) {
+    if (task.lesson == id) {
+      var $row = $('<tr>').addClass('row');
+      $row.append($('<td>').addClass('date')
+        .append($('<a>').text(shortdate(task.limit)).attr('href', '#list=' + formatdate(task.limit))));
+      $row.append($('<td>').addClass('limit').text(formattime(task.limit)));
+      $row.append($('<td>').addClass('teacher').text(task.teacher));
+      var $option = $('<td>').addClass('option');
+      $option.append($('<div>').addClass('task').text(task.task));
+      if (task.memo) {
+        $option.append($('<div>').addClass('memo').text('メモ').on('click', function () {
+          alert(task.memo);
+        }));
       }
-    })(schedules[index]);
+      $row.append($option);
+      $table.append($row);
+    }
+  };
+
+  var taskindex = 0;
+  var task = tasks && tasks[taskindex++];
+  var $table = $('<table>').addClass('lesson-list');
+  $.each(schedules, function (index, data) {
+    if (data.lesson == id) {
+      while (task && task.limit < data.date) {
+        addtask(task);
+        task = tasks[taskindex++];
+      }
+
+      var $row = $('<tr>').addClass('row');
+      $row.append($('<td>').addClass('date').append($('<a>').text(shortdate(data.date)).attr('href', '#list=' + data.date)));
+      $row.append($('<td>').addClass('period').text(formatperiod(data)));
+      $row.append($('<td>').addClass('teacher').text(data.teacher));
+      var $option = $('<td>').addClass('option');
+      if (data.exercises) {
+        $option.append($('<div>').addClass('exercises'));
+      }
+      if (data.test) {
+        $option.append($('<div>').addClass('test').text(data.test));
+      }
+      if (data.task) {
+        $option.append($('<div>').addClass('task').text(data.task));
+      }
+      if (data.memo) {
+        $option.append($('<div>').addClass('memo').text('メモ').on('click', function () {
+          alert(data.memo);
+        }));
+      }
+      $row.append($option);
+      $table.append($row);
+    }
+  });
+
+  while (task) {
+    addtask(task);
+    task = tasks[taskindex++];
   }
+
   $area.append($table);
   return $area;
 };
