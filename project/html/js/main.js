de2c0d9c8f21776690b162303fe104d95fe0238c
Tama Eguchi <tama.eguchi@gmail.com>
2016-04-20 19:27:34 +0900

    再読み込み対応とハッシュジャンプ整理

--- project/html/js/main.js
+++ project/html/js/main.js
@@ -11,75 +11,81 @@ var holidays = {
 };
 
 $(function () {
-  $.ajax({
-    url: 'api/alldata?' + new Date().getTime(),
-    success: function (data) {
-      lessons = {};
-      $.each(data.lesson, function (index, obj) {
-        lessons[obj.id] = obj;
-      });
-      schedules = data.schedule;
-      tasks = data.task;
-      //holidays = data.holiday;
-
-      // hashチェック
-      var currenthash;
-      var currentmode;
-      setInterval(function () {
-        if (currenthash != location.hash) {
-          currenthash = location.hash;
-          var params = currenthash.slice(1).split('=');
-          var mode = params[0] || 'calendar';
-          var option = params[1];
-
-          var $content = $('#content');
-          switch (mode) {
-            case 'calendar':
-              // カレンダー表示
-              if (currentmode != mode) {
-                $content.empty().append(makecalendar());
-              }
-              scrolldate(option);
-              break;
-
-            case 'list':
-              // リスト表示
-              if (currentmode != mode) {
-                $content.empty().append(makelist());
-              }
-              scrolldate(option);
-              break;
-
-            case 'task':
-              // タスク
-              $content.empty().append(makelist(true));
-              scrolldate(option);
-              break;
-
-            case 'lesson':
-              // 授業表示
-              $content.empty().append(makelesson(option));
-              break;
-          }
-          currentmode = mode;
-        }
-      }, 100);
-    },
+  $('#reload').on('click', function () {
+    reload();
+    return false;
   });
+
+  var reload = function () {
+    $.ajax({
+      url: 'api/alldata?' + new Date().getTime(),
+      success: function (data) {
+        lessons = {};
+        $.each(data.lesson, function (index, obj) {
+          lessons[obj.id] = obj;
+        });
+        schedules = data.schedule;
+        tasks = data.task;
+        //holidays = data.holiday;
+        setpage();
+      },
+    });
+  };
+  reload();
+
+  var currenthash;
+  var currentmode;
+  var setpage = function (hash) {
+    currenthash = hash || location.hash;
+    var params = currenthash.slice(1).split('=');
+    var mode = params[0] || 'calendar';
+    var option = params[1];
+
+    var $content = $('#content');
+    switch (mode) {
+      case 'calendar':
+        // カレンダー表示
+        $content.empty().append(makecalendar());
+        scrolldate(option);
+        break;
+
+      case 'list':
+        // リスト表示
+        $content.empty().append(makelist());
+        scrolldate(option);
+        break;
+
+      case 'task':
+        // タスク
+        $content.empty().append(makelist(true));
+        scrolldate(option);
+        break;
+
+      case 'lesson':
+        // 授業表示
+        $content.empty().append(makelesson(option));
+        break;
+    }
+    currentmode = mode;
+  };
+
+  // hashチェック
+  setInterval(function () {
+    if (lessons && currenthash != location.hash) {
+      setpage();
+    }
+  }, 100);
 
   var scrolldate = function (option) {
     // 日付存在チェック
-    var target = option || formatdate(new Date());
-    $.each(schedules, function (index, obj) {
-      var date = obj.date;
-      if (date >= target) {
-        target = date;
-        return false;
+    var date = option ? new Date(option) : new Date();
+    for (var count = 0; count < 10; count++) {
+      var $target = $('#' + formatdate(date));
+      if ($target.length) {
+        window.scrollTo(0, $target.offset().top - parseInt($('#content').css('padding-top')));
+        return;
       }
-    });
-    var $target = $('#' + target);
-    if ($target.length > 0) {
-      window.scrollTo(0, $target.offset().top - parseInt($('#content').css('padding-top')));
+      date.setDate(date.getDate() + 1);
     }
   };
 });
