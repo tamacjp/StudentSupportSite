008c126c4cbb005d3c246619c9f3271cd5178993
Tama Eguchi <tama.eguchi@gmail.com>
2016-04-25 19:51:39 +0900

    プロジェクト名修正

--- project/sss/wsgi.py
+++ project/sss/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for omcn6 project.
+WSGI config for sss project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "omcn6.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sss.settings")
 
 application = get_wsgi_application()
